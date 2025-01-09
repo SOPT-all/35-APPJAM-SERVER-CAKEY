859778b0217ead12a2652a2b3599de7464c597fd
Kwak Seong Joon <seongjoonkwak@inu.ac.kr>
2025-01-10 03:54:04 +0900

    setting: 멀티모듈 프로젝트 세팅 - #2

---
+++ cakey-api/src/main/java/com/cakey/CakeyServerApplication.java
@@ -0,0 +1,11 @@
+package com.cakey;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CakeyServerApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(CakeyServerApplication.class, args);
+    }
+}
