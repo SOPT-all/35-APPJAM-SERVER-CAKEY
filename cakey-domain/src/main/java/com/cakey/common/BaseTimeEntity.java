1a380baf297f0cd49f11ee7824bd9bf7193d4548
Kwak Seong Joon <seongjoonkwak@inu.ac.kr>
2025-01-12 01:34:21 +0900

    setting: BaseTimeEntity 세팅 - #4

---
+++ cakey-domain/src/main/java/com/cakey/common/BaseTimeEntity.java
@@ -0,0 +1,19 @@
+package com.cakey.common;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.EntityListeners;
+import jakarta.persistence.MappedSuperclass;
+import lombok.Getter;
+import org.springframework.data.annotation.CreatedDate;
+import org.springframework.data.jpa.domain.support.AuditingEntityListener;
+
+import java.time.LocalDateTime;
+
+@Getter
+@EntityListeners(AuditingEntityListener.class)
+@MappedSuperclass
+public abstract class BaseTimeEntity {
+    @Column(updatable = false)
+    @CreatedDate
+    private LocalDateTime createdDate;
+}
