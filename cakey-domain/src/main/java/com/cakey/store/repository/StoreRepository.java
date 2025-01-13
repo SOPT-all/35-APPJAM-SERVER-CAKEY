762a67be48f9f27cabe25520ceb5281dea6d8e07
Kwak Seong Joon <seongjoonkwak@inu.ac.kr>
2025-01-13 18:16:19 +0900

    feat: storeRepository 생성 - #9

---
+++ cakey-domain/src/main/java/com/cakey/store/repository/StoreRepository.java
@@ -0,0 +1,7 @@
+package com.cakey.store.repository;
+
+import com.cakey.store.domain.Store;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface StoreRepository extends JpaRepository<Store, Long> {
+}
