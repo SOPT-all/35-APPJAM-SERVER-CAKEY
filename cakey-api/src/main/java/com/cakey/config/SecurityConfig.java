e224bbc2002b49a341bc5ca8f4bf225e04e742f1
Kwak Seong Joon <seongjoonkwak@inu.ac.kr>
2025-01-15 04:03:45 +0900

    fix: fix - #16

--- cakey-api/src/main/java/com/cakey/config/SecurityConfig.java
+++ cakey-api/src/main/java/com/cakey/config/SecurityConfig.java
@@ -1,78 +1,78 @@
-package com.cakey.config;
-
-import com.cakey.common.auth.CustomAccessDeniedHandler;
-import com.cakey.common.auth.CustomJwtAuthenticationEntryPoint;
-import com.cakey.common.auth.JwtTokenProvider;
-import com.cakey.common.auth.filter.CustomAuthenticationFilter;
-import com.cakey.common.auth.filter.JwtAuthenticationFilter;
-import lombok.RequiredArgsConstructor;
-import org.springframework.boot.web.servlet.FilterRegistrationBean;
-import org.springframework.context.annotation.Bean;
-import org.springframework.context.annotation.Configuration;
-import org.springframework.security.config.annotation.web.builders.HttpSecurity;
-import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
-import org.springframework.security.config.annotation.web.configuration.WebSecurityCustomizer;
-import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
-import org.springframework.security.config.http.SessionCreationPolicy;
-import org.springframework.security.web.SecurityFilterChain;
-import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
-
-@Configuration
-@EnableWebSecurity
-@RequiredArgsConstructor
-public class SecurityConfig {
-
-    private final JwtAuthenticationFilter jwtAuthenticationFilter;
-    private final CustomAuthenticationFilter customAuthenticationFilter;
-    private final CustomAccessDeniedHandler customAccessDeniedHandler;
-    private final CustomJwtAuthenticationEntryPoint customJwtAuthenticationEntryPoint;
-    private final JwtTokenProvider jwtTokenProvider;
-
-    private static final String[] AUTH_WHITELIST = {
-            "/actuator/health",
-            "/api/v1/user/login",
-            "/token-refresh",
-            "/api/v1/user/hi",
-    };
-
-    @Bean
-    public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
-        return http.csrf(AbstractHttpConfigurer::disable)
-                .formLogin(AbstractHttpConfigurer::disable)
-                .httpBasic(AbstractHttpConfigurer::disable)
-                .sessionManagement(
-                        session -> session.sessionCreationPolicy(SessionCreationPolicy.STATELESS))
-                .exceptionHandling(
-                        exception -> exception.authenticationEntryPoint(customJwtAuthenticationEntryPoint)
-                                .accessDeniedHandler(customAccessDeniedHandler))
-                .authorizeHttpRequests(auth -> auth
-                        .requestMatchers(AUTH_WHITELIST).permitAll() // 화이트리스트 경로 허용
-                        .anyRequest().authenticated())
-                .addFilterBefore(new JwtAuthenticationFilter(jwtTokenProvider), UsernamePasswordAuthenticationFilter.class)
-                .addFilterBefore(new CustomAuthenticationFilter(jwtTokenProvider), UsernamePasswordAuthenticationFilter.class)
-                .build();
-    }
-
-    @Bean
-    public FilterRegistrationBean<JwtAuthenticationFilter> firstFilterFilterRegistrationBean() {
-        FilterRegistrationBean<JwtAuthenticationFilter> registrationBean = new FilterRegistrationBean<>();
-        registrationBean.setFilter(jwtAuthenticationFilter);
-        registrationBean.addUrlPatterns("/api/v1/user/test1"); // /test1 경로에만 FirstFilter 적용
-        registrationBean.setOrder(1); // 필터 순서 설정
-        return registrationBean;
-    }
-
-    @Bean
-    public FilterRegistrationBean<CustomAuthenticationFilter> secondFilterFilterRegistrationBean() {
-        FilterRegistrationBean<CustomAuthenticationFilter> registrationBean = new FilterRegistrationBean<>();
-        registrationBean.setFilter(customAuthenticationFilter);
-        registrationBean.addUrlPatterns("/v1/user/test2"); // /test2 경로에만 SecondFilter 적용
-        registrationBean.setOrder(2); // 필터 순서 설정
-        return registrationBean;
-    }
-
-    @Bean
-    public WebSecurityCustomizer webSecurityCustomizer() {
-        return web -> web.ignoring().requestMatchers(AUTH_WHITELIST);
-    }
-}
+//package com.cakey.config;
+//
+//import com.cakey.common.auth.CustomAccessDeniedHandler;
+//import com.cakey.common.auth.CustomJwtAuthenticationEntryPoint;
+//import com.cakey.common.auth.JwtTokenProvider;
+//import com.cakey.common.auth.filter.CustomAuthenticationFilter;
+//import com.cakey.common.auth.filter.JwtAuthenticationFilter;
+//import lombok.RequiredArgsConstructor;
+//import org.springframework.boot.web.servlet.FilterRegistrationBean;
+//import org.springframework.context.annotation.Bean;
+//import org.springframework.context.annotation.Configuration;
+//import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+//import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+//import org.springframework.security.config.annotation.web.configuration.WebSecurityCustomizer;
+//import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
+//import org.springframework.security.config.http.SessionCreationPolicy;
+//import org.springframework.security.web.SecurityFilterChain;
+//import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
+//
+//@Configuration
+//@EnableWebSecurity
+//@RequiredArgsConstructor
+//public class SecurityConfig {
+//
+//    private final JwtAuthenticationFilter jwtAuthenticationFilter;
+//    private final CustomAuthenticationFilter customAuthenticationFilter;
+//    private final CustomAccessDeniedHandler customAccessDeniedHandler;
+//    private final CustomJwtAuthenticationEntryPoint customJwtAuthenticationEntryPoint;
+//    private final JwtTokenProvider jwtTokenProvider;
+//
+//    private static final String[] AUTH_WHITELIST = {
+//            "/actuator/health",
+//            "/api/v1/user/login",
+//            "/token-refresh",
+//            "/api/v1/user/hi",
+//    };
+//
+//    @Bean
+//    public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
+//        return http.csrf(AbstractHttpConfigurer::disable)
+//                .formLogin(AbstractHttpConfigurer::disable)
+//                .httpBasic(AbstractHttpConfigurer::disable)
+//                .sessionManagement(
+//                        session -> session.sessionCreationPolicy(SessionCreationPolicy.STATELESS))
+//                .exceptionHandling(
+//                        exception -> exception.authenticationEntryPoint(customJwtAuthenticationEntryPoint)
+//                                .accessDeniedHandler(customAccessDeniedHandler))
+//                .authorizeHttpRequests(auth -> auth
+//                        .requestMatchers(AUTH_WHITELIST).permitAll() // 화이트리스트 경로 허용
+//                        .anyRequest().authenticated())
+//                .addFilterBefore(new JwtAuthenticationFilter(jwtTokenProvider), UsernamePasswordAuthenticationFilter.class)
+//                .addFilterBefore(new CustomAuthenticationFilter(jwtTokenProvider), UsernamePasswordAuthenticationFilter.class)
+//                .build();
+//    }
+//
+//    @Bean
+//    public FilterRegistrationBean<JwtAuthenticationFilter> firstFilterFilterRegistrationBean() {
+//        FilterRegistrationBean<JwtAuthenticationFilter> registrationBean = new FilterRegistrationBean<>();
+//        registrationBean.setFilter(jwtAuthenticationFilter);
+//        registrationBean.addUrlPatterns("/api/v1/user/test1"); // /test1 경로에만 FirstFilter 적용
+//        registrationBean.setOrder(1); // 필터 순서 설정
+//        return registrationBean;
+//    }
+//
+//    @Bean
+//    public FilterRegistrationBean<CustomAuthenticationFilter> secondFilterFilterRegistrationBean() {
+//        FilterRegistrationBean<CustomAuthenticationFilter> registrationBean = new FilterRegistrationBean<>();
+//        registrationBean.setFilter(customAuthenticationFilter);
+//        registrationBean.addUrlPatterns("/v1/user/test2"); // /test2 경로에만 SecondFilter 적용
+//        registrationBean.setOrder(2); // 필터 순서 설정
+//        return registrationBean;
+//    }
+//
+//    @Bean
+//    public WebSecurityCustomizer webSecurityCustomizer() {
+//        return web -> web.ignoring().requestMatchers(AUTH_WHITELIST);
+//    }
+//}
