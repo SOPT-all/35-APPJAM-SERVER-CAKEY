0a9bae0ed79f8ce916c1e2db96b90d3aabc5c745
Yerin Lee <91695537+lxxyxin@users.noreply.github.com>
2025-01-16 00:11:05 +0900

    feat : 디자인 좋아요 여부 검증 로직 구현 - #29

---
+++ cakey-domain/src/main/java/com/cakey/cakelike/repository/CakeLikesRepository.java
@@ -0,0 +1,8 @@
+package com.cakey.cakelike.repository;
+
+import com.cakey.cakelike.domain.CakeLikes;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface CakeLikesRepository extends JpaRepository<CakeLikes, Long> {
+    boolean existsByCakeIdAndUserId(final Long cakeId, final Long userId);
+}
