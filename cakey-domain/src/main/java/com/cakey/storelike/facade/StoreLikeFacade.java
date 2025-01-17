85fbbd751ccef1a95c63945cd64890c7bc5b03cd
Kwak Seong Joon <seongjoonkwak@inu.ac.kr>
2025-01-18 00:53:35 +0900

    chore: 클래스명 변경 - #65

--- cakey-domain/src/main/java/com/cakey/storelike/facade/StoreLikeFacade.java
+++ cakey-domain/src/main/java/com/cakey/storelike/facade/StoreLikeFacade.java
@@ -3,20 +3,18 @@ package com.cakey.storelike.facade;
 import com.cakey.storelike.domain.StoreLike;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Component;
-import org.springframework.transaction.annotation.Transactional;
 
 @Component
 @RequiredArgsConstructor
 public class StoreLikeFacade {
     private final StoreLikeRetriever storeLikeRetriever;
-    private final StoreLikesCreator storeLikeCreator;
+    private final StoreLikesSaver storeLikeCreator;
 
     public int countAllLikedStoreByUserId(final long userId) {
         return storeLikeRetriever.coundAllLikedStoreByUserId(userId);
     }
 
     //스토어 좋아요 등록
-    @Transactional
     public void saveStoreLikes(final long userId, final long storeId) {
         final StoreLike newStoreLikes = StoreLike.createStoreLike(storeId, userId);
         storeLikeCreator.saveStoreLikes(newStoreLikes);
--- cakey-domain/src/main/java/com/cakey/storelike/facade/StoreLikesSaver.java
+++ cakey-domain/src/main/java/com/cakey/storelike/facade/StoreLikesSaver.java
@@ -4,12 +4,14 @@ import com.cakey.storelike.domain.StoreLike;
 import com.cakey.storelike.repository.StoreLikeRepository;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Component;
+import org.springframework.transaction.annotation.Transactional;
 
 @Component
 @RequiredArgsConstructor
-public class StoreLikesCreator {
+public class StoreLikesSaver {
     private final StoreLikeRepository storeLikeRepository;
 
+    @Transactional
     public void saveStoreLikes(final StoreLike storeLike) {
         storeLikeRepository.save(storeLike);
     }
