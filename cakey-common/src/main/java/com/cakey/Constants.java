b2eb834c120be2139b05b1c9494fd26051bec851
Kwak Seong Joon <seongjoonkwak@inu.ac.kr>
2025-01-24 09:21:33 +0900

    feat: filter에서 jwt 사용 - #154

--- cakey-common/src/main/java/com/cakey/Constants.java
+++ cakey-common/src/main/java/com/cakey/Constants.java
@@ -4,5 +4,6 @@ public abstract class Constants {
     public static final String CHARACTER_TYPE = "utf-8";
     public static final String BEARER = "Bearer ";
     public static final String AUTHCODE = "authorization_code";
+    public static final String AUTHORIZATION = "Authorization";
 
 }
