6a009c2c5f284747af50c62c3cf10de3ff22a7e2
Kwak Seong Joon <seongjoonkwak@inu.ac.kr>
2025-03-18 23:30:18 +0900

    chore: 형식 변경 - #172

--- cakey-common/src/main/java/com/cakey/rescode/EventCode.java
+++ cakey-common/src/main/java/com/cakey/rescode/EventCode.java
@@ -7,7 +7,7 @@ import lombok.RequiredArgsConstructor;
 @Getter
 public enum EventCode {
     DISCORD_SIGNUP_EVENT(
-            "[🍰 Whipee 신규 유저 가입 🍰]\n1)이름: [%s]\n2)현재 가입자 수: [%d]"),
+            "[🍰 Whipee 신규 유저 가입 🍰]\n1) 이름: [%s]\n2) 현재 가입자 수: [%d]"),
     ;
 
     private final String eventMessage;
